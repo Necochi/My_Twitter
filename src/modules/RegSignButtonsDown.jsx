f3dfccbf71a3d9ee8e8119bbbf682a73a6bbdc30
Necochi <LikaGirl0@gmail.com>
2024-07-04 23:53:11 +0300

    updated blur

--- src/modules/RegSignButtonsDown.jsx
+++ src/modules/RegSignButtonsDown.jsx
@@ -4,20 +4,9 @@ import { showRegForm } from "../store/slices/regFormSlice";
 import { showSignForm } from "../store/slices/signFormSlice";
 const RegSignButtonsDown = () => {
   const dispatch = useDispatch();
-  const regState = useSelector((state) => state.regForm.isHidden);
-  const signState = useSelector((state) => state.signForm.isHidden);
-  let newColor = {};
-
-  if (!regState || !signState) {
-    newColor = {
-      background: "linear-gradient(#00000000, #626262)",
-    };
-  }
 
   return (
     <>
-      <div className={style.blur_div}
-      style={newColor}></div>
       <div className={style.register_up}>
         <p>Зарегистрируйтесь и узнайте обо всём первым</p>
         <div className={style.buttons}>
