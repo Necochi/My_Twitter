a02bf051c9462eb2abe40e859058071b5b2c7010
Necochi <LikaGirl0@gmail.com>
2024-07-08 20:29:29 +0300

    updated app.get

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/date', (req, res) => res.type('json').send({ date: new Date() }));
+app.get("/", (req, res) => res.type("html").send(html));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
