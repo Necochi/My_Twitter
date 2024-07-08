b98229e5213e46a4eb3d6f03fc73077a5bc71adc
Necochi <LikaGirl0@gmail.com>
2024-07-08 20:02:56 +0300

    added express

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './public',
+    outDir: "./public",
     emptyOutDir: false,
   },
   plugins: [react()],
+  server: {
+    proxy: {
+      "/date": "http://localhost:3000",
+    },
+  },
 });
