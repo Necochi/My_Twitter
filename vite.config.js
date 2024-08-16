b2636564acd85ed9c4470b3ed3ee53fc06feb756
Necochi <LikaGirl0@gmail.com>
2024-08-16 22:00:48 +0300

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './public',
+    outDir: "./public",
     emptyOutDir: false,
   },
   plugins: [react()],
   server: {
     proxy: {
-      '/date': 'http://localhost:3000',
-      '/posts': 'http://localhost:3000',
-      '/createUser': 'http://localhost:3000',
-      '/login': 'http://localhost:3000',
-      '/checking-token': 'http://localhost:3000',
+      "/date": "http://localhost:3000",
+      "/posts": "http://localhost:3000",
+      "/createUser": "http://localhost:3000",
+      "/login": "http://localhost:3000",
+      "/checking-token": "http://localhost:3000",
     },
   },
 });
