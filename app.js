6427f80b00152f3f73e75b8fd09259d870225967
LikaLaptop <likagirl0@gmail.com>
2025-11-07 16:13:12 +0300

    new poryadok

--- app.js
+++ app.js
@@ -18,9 +18,14 @@ const client = new Client({
 });
 await client.connect();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+
+const currentFile = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentFile);
+
+app.use(express.static(path.join(currentDir, 'public')));
 
 client.keepAliveTimeout = 120000;
 client.headersTimeout = 120000;
@@ -452,15 +457,10 @@ app.post('/changeMail', async (req, res) => {
   return res.status(400).json('Произошла какая-то ошибка');
 });
 
-const currentFile = fileURLToPath(import.meta.url);
-const currentDir = path.dirname(currentFile);
-
 app.use((req, res, next) => {
   console.log('➡️  Запрос:', req.method, req.path);
   next();
 });
-
-app.use(express.static(path.join(currentDir, 'public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(currentDir, 'public', 'index.html'));
